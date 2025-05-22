bf6f96efa024059d4be78dedc709e6670281559e
Wisit Tangtakoune <wisit.tangtakoune@gmail.com>
2025-05-22 05:50:01 +0000

    added functions to generate computer choice and another to get input

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function getComputerChoice () {
-
+function getComputerChoice() {
+    let choice = Math.random() * 3;
+    console.log(choice);
+    if (choice <= 1) {
+        return "rock";
+    } else if (choice <= 2) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
 };
+
+function getHumanChoice() {
+    let word = prompt("Pick rock, paper, or scissors:")
+    let value = (word.toUpperCase()).toLowerCase();
+   //let value = prompt("Enter rock, paper, or scissors:");
+    return console.log(value);
+}
+
+getHumanChoice();
