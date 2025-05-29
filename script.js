30dfe0b52247e6939685f0f89338a0db2fd71d54
Wisit Tangtakoune <wisit.tangtakoune@gmail.com>
2025-05-29 06:45:22 +0000

    Added styles page, edited js of the page.

--- script.js
+++ script.js
@@ -16,30 +16,34 @@ function getHumanChoice() {
     return value;
 };
 
-function playGame() {
-    let humanScore = 0, computerScore = 0;
-    function playRound(comp, human) {
-        human = getHumanChoice();
-        comp = getComputerChoice();
-        if (human === comp) {
-            return console.log(`computer chose ${comp}, you tied`);
-        } else if ((human == "scissors" && comp == "rock") || (human == "rock" && comp == "paper") || (human == "paper" && comp == "scissors")) {
-            ++computerScore;
-            return console.log(`computer chose ${comp}, you lost. Computer gained 1 point.`);
-        } else {
-            ++humanScore;
-            return console.log(`computer chose ${comp}, you won. You've gained 1 point.`);
-        }
-    };
-    for (let i = 0; i < 5; ++i) {
-        playRound();
-    } if (humanScore == computerScore) {
-        return console.log(`Total score was you: ${humanScore}, us: ${computerScore}.You tied.`);
-    } else if (humanScore > computerScore) {
-        return console.log(`Total score was you: ${humanScore}, us: ${computerScore}. You won!`);
-    } else {
-        return console.log(`Total score was you: ${humanScore}, us: ${computerScore}.You lost!`);
-    }
-};
+const body = document.body;
+const infoText = document.createElement("div");
+infoText.textContent ="To play a game of Rock-Paper-Scissors click on a button."
+body.insertBefore("infoText", "container");
 
-playGame();
+// function playGame() {
+//     let humanScore = 0, computerScore = 0;
+//     function playRound(comp, human) {
+//         human = getHumanChoice();
+//         comp = getComputerChoice();
+//         if (human === comp) {
+//             return console.log(`computer chose ${comp}, you tied`);
+//         } else if ((human == "scissors" && comp == "rock") || (human == "rock" && comp == "paper") || (human == "paper" && comp == "scissors")) {
+//             ++computerScore;
+//             return console.log(`computer chose ${comp}, you lost. Computer gained 1 point.`);
+//         } else {
+//             ++humanScore;
+//             return console.log(`computer chose ${comp}, you won. You've gained 1 point.`);
+//         }
+//     };
+//     for (let i = 0; i < 5; ++i) {
+//         playRound();
+//     } if (humanScore == computerScore) {
+//         return console.log(`Total score was you: ${humanScore}, us: ${computerScore}.You tied.`);
+//     } else if (humanScore > computerScore) {
+//         return console.log(`Total score was you: ${humanScore}, us: ${computerScore}. You won!`);
+//     } else {
+//         return console.log(`Total score was you: ${humanScore}, us: ${computerScore}.You lost!`);
+//     }
+// };
+
